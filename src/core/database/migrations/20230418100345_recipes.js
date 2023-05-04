5c7e14bc29945f34738d1164e20f2b09012a23ec
Hoang Quang Hung <79563371+hunine@users.noreply.github.com>
2023-05-04 21:39:13 +0700

    SMF-8: Database (#2)

---
+++ src/core/database/migrations/20230418100345_recipes.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+const tableName = 'recipes';
+
+exports.up = async knex => {
+    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        .createTable(tableName, table => {
+            table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('uuid_generate_v4()'));
+            table.string('name');
+            table.string('level');
+            table.string('description');
+            table.dateTime('deleted_at').defaultTo(null);
+            table.timestamps(false, true);
+        });
+
+    await knex.raw(`
+   CREATE TRIGGER update_timestamp
+   BEFORE UPDATE
+   ON ${tableName}
+   FOR EACH ROW
+   EXECUTE PROCEDURE update_timestamp();
+ `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable(tableName);
---
+++ src/core/database/migrations/20230424022938_cuisine.js
@@ -0,0 +1,30 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+const tableName = 'cuisine';
+
+exports.up = async knex => {
+    await knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+        .createTable(tableName, table => {
+            table.uuid('id', { primaryKey: true }).defaultTo(knex.raw('uuid_generate_v4()'));
+            table.string('name');
+            table.dateTime('deleted_at').defaultTo(null);
+            table.timestamps(false, true);
+        });
+
+    await knex.raw(`
+   CREATE TRIGGER update_timestamp
+   BEFORE UPDATE
+   ON ${tableName}
+   FOR EACH ROW
+   EXECUTE PROCEDURE update_timestamp();
+ `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = knex => knex.schema.dropTable(tableName);

50ffbcc295bd64dc85b1c49b19659c9033dfe7ba
Hunine <hqhung201@gmail.com>
2023-05-09 07:02:24 +0700

    SMF-13: Join table when get recipe

--- src/core/modules/recipes/repository/recipe.repository.js
+++ src/core/modules/recipes/repository/recipe.repository.js
@@ -3,9 +3,21 @@ import { DataRepository } from 'packages/restBuilder/core/dataHandler';
 class Repository extends DataRepository {
     findById(id) {
         return this.query()
+            .innerJoin('recipes_images', 'recipes.id', 'recipes_images.recipe_id')
+            .innerJoin('images', 'recipes_images.image_id', 'images.id')
+            .leftJoin('levels', 'recipes.level_id', 'levels.id')
             .whereNull('recipes.deleted_at')
             .whereRaw(`recipes.id::text = '${id}'`)
-            .select()
+            .select(
+                'recipes.id',
+                'recipes.name',
+                'levels.name as level',
+                'images.url as image_url',
+                'recipes.description',
+                'recipes.deleted_at',
+                'recipes.created_at',
+                'recipes.updated_at',
+            )
             .first();
     }
 }
